19e4f406617e605009b20422837ece24de13c205
Yohan Lussiez <yohan.lussiez@outlook.fr>
2024-06-12 16:58:42 +0200

    First commit

---
+++ src/main/java/org/example/devicemonitoringservice/DeviceMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.devicemonitoringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMonitoringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceMonitoringServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/devicemonitoringservice/DeviceMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devicemonitoringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMonitoringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
